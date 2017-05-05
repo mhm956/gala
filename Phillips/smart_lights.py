2dd078d09cb1ffd43291d9c6df50f7684bc0e754
mhm956 <mhm956@my.utsa.edu>
2017-05-04 22:36:17 -0500

    * Added example that does the following: 1) Take in a string from the keyboard 2) Sends the string to API.AI for AI interpretation 3) Receives and parses the JSON 4) Activates the hue lights accordingly 5) Responds to the user * Removed some unneeded files

---
+++ Phillips/smart_lights.py
@@ -0,0 +1,42 @@
+from phue import Bridge
+
+b = Bridge('192.168.0.103')
+
+# If running for the first time, press button on bridge and run with b.connect() uncommented
+# b.connect()
+
+
+def phue_lights(color=None, light_state=None, room=None):
+    action_flag = False
+    if room:
+
+        uuid = int(b.get_light_id_by_name(room))
+        if light_state == "on":
+            b.set_light(uuid, 'on', True)
+            action_flag = True
+        elif light_state == "off":
+            b.set_light(uuid, 'on', False)
+            action_flag = True
+        elif light_state == "dim":
+            b.set_light(uuid, 'bri', 0)
+            action_flag = True
+        else:
+            pass
+
+        if color:
+            if color == 'red':
+                b.set_light(uuid, 'hue', 0)
+                action_flag = True
+            elif color == 'orange':
+                b.set_light(uuid, 'hue', 8000)
+                action_flag = True
+            elif color == 'blue':
+                b.set_light(uuid, 'hue', 46920)
+                action_flag = True
+            elif color == 'green':
+                b.set_light(uuid, 'hue', 22500)
+                action_flag = True
+            else:
+                pass
+
+    return action_flag
